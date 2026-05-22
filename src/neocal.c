f5464b3cb114da2b01355a945bcabfe16ea57eb0
Ayobamidele <ojasanwobamidele@gmail.com>
2026-05-22 19:47:48 +0100

    added some mathematical functions

--- src/neocal.c
+++ src/neocal.c
@@ -1,4 +1,37 @@
 #include<stdio.h>
+#include<math.h>
+void power(){double a,b,c;
+	printf("enter the first numberand second number:" );
+	scanf("%lf  %lf",&a,&b);
+	c = pow(a,b);
+	printf("%lf raised to the power %lf is %lf",a,b,c);}
+void squareroot(){double a,c;
+	 printf("enter the  number: ");
+    scanf("%lf", &a);
+	c = sqrt(a);
+    printf("the square-root  of %lf is %lf\n", a, c);}
+void absolutevalue(){double  a,c;
+   printf("enter the  number: ");
+    scanf("%lf", &a);
+        c = fabs(a);
+    printf("The absolute value  of %lf is %lf\n", a, c);}
+void  roundup(){double a,c;
+         printf("enter the number: ");
+    	scanf("%lf", &a);
+        c = ceil(a);
+    printf("the rounding up of %lf to the nearest interger  is %lf\n", a, c);
+}
+void rounddown(){double a,c;
+         printf("enter the number: ");
+        scanf("%lf", &a);
+        c = floor(a);
+    printf("the rounding down of %lf to the nearest interger  is %lf\n", a, c);}
+void rounding(){double a,c;
+         printf("enter the number: ");
+        scanf("%lf", &a);
+        c = round(a);
+    printf("the rounding of %lf to the nearest interger  is %lf\n", a, c);
+}
 void multiply(){int a,b;
      printf("enter two numbers: ");
     scanf("%d %d", &a, &b);
@@ -26,7 +59,8 @@ int main(){int option,menu;
     printf("Welcome to neocal\n");
     printf("enter 1 to show menu: ");
     scanf("%d", &menu);
-    printf("MENU\nchoose an option\n1. Addition\t\t2.subtraction\n3. Multiplication\t4. Division\n");
+    printf("MENU\nchoose an option\n1. Addition\t\t2. subtraction\n3. Multiplication\t4. Division\n5. Power\t\t6. Square-root\n7. Absolute-value\t8. Round-up\n9. Round-down\t\t10. Round\n");
+;
     scanf("%d", &option);
     if(option==1){
         addition();
@@ -35,7 +69,13 @@ int main(){int option,menu;
     }else if(option==4){ division();}
     else if(option==3){
         multiply();
-    }else{
+    }else if(option==5){power();}
+	else if(option==6){squareroot();}
+	else if(option==7){absolutevalue();}
+	else if(option==8){roundup();}
+	else if(option==9){rounddown();}
+	else if(option==10){rounding();}
+	else{
         printf("invalid option\n");
     }
     return 0;   }
