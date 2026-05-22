95e574d94968e28415f0fd529fafec8c72bf8d09
Ayobamidele <ojasanwobamidele@gmail.com>
2026-05-22 15:30:28 +0100

    good

---
+++ src/README.md
@@ -0,0 +1,2 @@
+# NeoCal
+A clean, from-scratch calculator built in C on Linux. Daily learning journey in C, Linux, and systems programming. Documented on TikTok.
--- src/hello.c
+++ src/hello.c
@@ -5,9 +5,9 @@ printf("enter your name: \n");
 fgets(name,50,stdin);//takes aall the inputs and prits the ignoring all the spaces//
 printf("wlecome to the c programming world %s\n",name);
 char name1[50] ; 
-//to use   the scanf the takes     the \n as the enter kry or eof//
+//to use   the scanf the takes     the \n as the enter key or eof//
 printf("enter your name: \n");
 scanf("%s",name1);//takes only the first word and ignores the rest of the words//
-printf("welcome to the c programming world %s\n",name1);
+printf("welcome to the c programming world %s we hope you are ready for the upcming adventure you will be embarking on\n",name1);
 return 0;
 }
